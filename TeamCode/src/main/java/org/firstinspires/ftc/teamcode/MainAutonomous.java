c0c9d9f075e6660ffeefb54cd9a996e10ab5f5fb
Devam (Dodesimo) <devammondal06@gmail.com>
2021-11-22 18:42:42 -0500

    mf forgot to plug in the encoders

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MainAutonomous.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MainAutonomous.java
@@ -1,10 +1,10 @@
 package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.hardware.bosch.BNO055IMU;
+import com.qualcomm.hardware.bosch.JustLoggingAccelerationIntegrator;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
-import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.util.ElapsedTime;
 
 import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
@@ -12,44 +12,116 @@ import org.firstinspires.ftc.robotcore.external.navigation.AxesOrder;
 import org.firstinspires.ftc.robotcore.external.navigation.AxesReference;
 import org.firstinspires.ftc.robotcore.external.navigation.Orientation;
 
-@Autonomous(name = "MainAutonomous")
+
+@Autonomous(name = "AutoIMUEncoder")
+
+//obsolete branch because Krithish is bobo.
+
 public class MainAutonomous extends LinearOpMode {
 
-
-    private DcMotor frontLeft;
-    private DcMotor frontRight;
-    private DcMotor backLeft;
-    private DcMotor backRight;
+    public DcMotor frontLeft;
+    public DcMotor frontRight;
+    public DcMotor backLeft;
+    public DcMotor backRight;
 
     BNO055IMU imu;
-    Orientation angles;
+    Orientation lastAngles = new Orientation();
+    double globalAngle;
+
+
+    private ElapsedTime runtime = new ElapsedTime();
+
+    static final double COUNTS_PER_MOTOR_REV = 537.6;
+    static final double DRIVE_GEAR_REDUCTION = 9.5;
+    static final double WHEEL_DIAMETER_INCHES = 4.75;
+    static final double COUNTS_PER_INCH = (COUNTS_PER_MOTOR_REV * DRIVE_GEAR_REDUCTION) /
+            (WHEEL_DIAMETER_INCHES * 3.1415);
+    static final double DRIVE_SPEED = 0.2;
+    static final double TURN_SPEED = 0.15;
+
 
     @Override
-    public void runOpMode() throws InterruptedException {
 
-        frontLeft = hardwareMap.get(DcMotor.class, "frontLeft");
-        frontRight = hardwareMap.get(DcMotor.class, "frontRight");
-        backLeft = hardwareMap.get(DcMotor.class, "backLeft");
-        backRight = hardwareMap.get(DcMotor.class, "backRight");
+
+    public void runOpMode() {
+
+        //Initialize the IMU and its parameters.
+        BNO055IMU.Parameters parameters = new BNO055IMU.Parameters();
+        parameters.angleUnit = BNO055IMU.AngleUnit.DEGREES;
+        parameters.accelUnit = BNO055IMU.AccelUnit.METERS_PERSEC_PERSEC;
+        parameters.calibrationDataFile = "BNO055IMUCalibration.json";
+        parameters.loggingEnabled = true;
+        parameters.loggingTag = "IMU";
+        parameters.accelerationIntegrationAlgorithm = new JustLoggingAccelerationIntegrator();
+
+        imu = hardwareMap.get(BNO055IMU.class, "imu");
+        imu.initialize(parameters);
+
+        frontLeft = hardwareMap.dcMotor.get("frontLeft");
+        frontRight = hardwareMap.dcMotor.get("frontRight");
+        backLeft = hardwareMap.dcMotor.get("backLeft");
+        backRight = hardwareMap.dcMotor.get("backRight");
 
         frontLeft.setDirection(DcMotor.Direction.REVERSE);
         backLeft.setDirection(DcMotor.Direction.REVERSE);
 
-        BNO055IMU.Parameters p = new BNO055IMU.Parameters();
-        p.angleUnit = BNO055IMU.AngleUnit.DEGREES;
+        frontLeft.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        frontRight.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        backLeft.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        backRight.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
 
-        //Hardware map (remember to set on phone)
-        imu = hardwareMap.get(BNO055IMU.class, "imu");
-        imu.initialize(p);
+
+        //The IMU does not initialize instantly. This makes it so the driver can see when they can push Play without errors.
+        telemetry.addData("Mode", "calibrating...");
+        telemetry.update();
+        while (!isStopRequested() && !imu.isGyroCalibrated()) {
+            sleep(50);
+            idle();
+        }
+
+
+        telemetry.addData("Status", "Resetting Encoders");
+        telemetry.update();
+
+        frontLeft.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        frontRight.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        backLeft.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        backRight.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+
+        frontLeft.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        frontRight.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        backLeft.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        backRight.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+
+        telemetry.addData("Path0", "Starting at %7d :%7d",
+                frontLeft.getCurrentPosition(),
+                frontRight.getCurrentPosition(),
+                backLeft.getCurrentPosition(),
+                backRight.getCurrentPosition());
+        telemetry.update();
+
+        //Tells the driver it is ok to start.
+        telemetry.addData("Mode", "waiting for start");
+        telemetry.addData("imu calib status", imu.getCalibrationStatus().toString());
+        telemetry.update();
+
+        //variable for how fast the robot willmove
+        double DRIVE_POWER = 0.5;
 
         waitForStart();
 
-        driveForward(0.5, 1000);
-        turnRight(90, 0.5);
+        encoderDrive(DRIVE_SPEED, 6, 6, 5.0);
+        rotate(90, 0.2);
+        encoderDrive(DRIVE_SPEED, -6, -6, 5.0);
 
+
+        telemetry.addData("Path", "Complete");
+        telemetry.update();
     }
 
-    public void driveForward(double power, long time) {
+
+    //Method for driving forward by TIME, not Encoder
+    public void driveByTime(double power, long time) {
 
         frontLeft.setPower(power);
         frontRight.setPower(power);
@@ -58,35 +130,173 @@ public class MainAutonomous extends LinearOpMode {
 
         sleep(time);
 
-        frontLeft.setPower(0);
-        frontRight.setPower(0);
-        backLeft.setPower(0);
-        backRight.setPower(0);
-
-        sleep(500);
-
+        frontLeft.setPower(0.0);
+        frontRight.setPower(0.0);
+        backLeft.setPower(0.0);
+        backRight.setPower(0.0);
     }
 
-    public void turnRight(int angle, double power) {
-
-        angles = imu.getAngularOrientation(AxesReference.INTRINSIC, AxesOrder.ZYX, AngleUnit.DEGREES);
-        sleep(500);
-
+    //Method for turning right, by TIME, not IMU
+    public void TurnRightByTime(double power, long time) {
         frontLeft.setPower(power);
-        backLeft.setPower(power);
         frontRight.setPower(-power);
-        backRight.setPower(-power);
+        sleep(time);
 
-        while (angles.firstAngle > -angle && !isStopRequested()) {
+        frontLeft.setPower(0);
+        frontRight.setPower(0);
+        sleep(250);
+    }
 
-            angles = imu.getAngularOrientation(AxesReference.INTRINSIC, AxesOrder.ZYX, AngleUnit.DEGREES);
+    //Method for turning left, by TIME, not IMU
+    public void TurnLeftByTime(double power, long time) {
+        frontLeft.setPower(-power);
+        frontRight.setPower(power);
+        sleep(time);
+
+        frontLeft.setPower(0);
+        frontRight.setPower(0);
+        sleep(250);
+    }
+
+    //Method for driving with encoder
+    public void encoderDrive(double speed,
+                             double leftInches, double rightInches,
+                             double timeoutS) {
+        int newLeftTarget;
+        int newRightTarget;
+
+        // Ensure that the opmode is still active
+        if (opModeIsActive()) {
+
+            // Determine new target position, and pass to motor controller
+            newLeftTarget = frontLeft.getCurrentPosition() + (int) (leftInches * COUNTS_PER_INCH);
+            newLeftTarget = backLeft.getCurrentPosition() + (int) (leftInches * COUNTS_PER_INCH);
+            newRightTarget = frontRight.getCurrentPosition() + (int) (rightInches * COUNTS_PER_INCH);
+            newRightTarget = backRight.getCurrentPosition() + (int) (rightInches * COUNTS_PER_INCH);
+            frontLeft.setTargetPosition(newLeftTarget);
+            frontRight.setTargetPosition(newRightTarget);
+            backLeft.setTargetPosition(newLeftTarget);
+            backRight.setTargetPosition(newRightTarget);
+
+            // Turn On RUN_TO_POSITION
+            frontLeft.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+            frontRight.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+            backLeft.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+            backRight.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+
+            // reset the timeout time and start motion.
+            runtime.reset();
+            frontLeft.setPower(Math.abs(speed));
+            frontRight.setPower(Math.abs(speed));
+            backLeft.setPower(Math.abs(speed));
+            backRight.setPower(Math.abs(speed));
+
+
+            while (opModeIsActive() &&
+                    (runtime.seconds() < timeoutS) &&
+                    (frontLeft.isBusy() && frontRight.isBusy() && backLeft.isBusy() && backRight.isBusy())) {
+
+                // Display it for the driver.
+                telemetry.addData("Path1", "Running to %7d :%7d", newLeftTarget, newRightTarget);
+                telemetry.addData("Path2", "Running at %7d :%7d",
+                        frontLeft.getCurrentPosition(),
+                        frontRight.getCurrentPosition(),
+                        backLeft.getCurrentPosition(),
+                        backRight.getCurrentPosition());
+                telemetry.update();
+            }
+
+
+            // Stop all motion;
+            frontLeft.setPower(0);
+            frontRight.setPower(0);
+            backLeft.setPower(0);
+            backRight.setPower(0);
+
+            // Turn off RUN_TO_POSITION
+            frontLeft.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+            frontRight.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+            backLeft.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+            backRight.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
 
         }
 
-        frontLeft.setPower(0);
-        backLeft.setPower(0);
-        frontRight.setPower(0);
-        backRight.setPower(0);
 
     }
+
+    //This method reads the IMU getting the angle. It automatically adjusts the angle so that it is between -180 and +180.
+    public double getAngle() {
+        Orientation angles = imu.getAngularOrientation(AxesReference.INTRINSIC, AxesOrder.ZYX, AngleUnit.DEGREES);
+
+        double deltaAngle = angles.firstAngle - lastAngles.firstAngle;
+
+        if (deltaAngle < -180)
+            deltaAngle += 360;
+        else if (deltaAngle > 180)
+            deltaAngle -= 360;
+
+        globalAngle += deltaAngle;
+
+        lastAngles = angles;
+
+        return globalAngle;
+    }
+
+    //The method turns the robot by a specific angle, -180 to +180.
+    public void rotate(int degrees, double power) {
+        double leftPower, rightPower;
+
+        resetAngle();
+
+        //if the degrees are less than 0, the robot will turn right
+        if (degrees < 0) {
+            leftPower = power;
+            rightPower = -power;
+        } else if (degrees > 0)//if greater than 0, turn left
+        {
+            leftPower = -power;
+            rightPower = power;
+        } else return;
+
+
+        //sets power to motors with negative signs properly assigned to make the robot go in the correct direction
+        frontLeft.setPower(leftPower);
+        frontRight.setPower(rightPower);
+
+        //Repeatedly check the IMU until the getAngle() function returns the value specified.
+        if (degrees < 0) {
+            while (opModeIsActive() && getAngle() == 0) {
+            }
+
+            while (opModeIsActive() && getAngle() > degrees) {
+            }
+        } else
+            while (opModeIsActive() && getAngle() < degrees) {
+            }
+
+
+        //stop the motors after the angle has been found.
+
+        frontLeft.setPower(0);
+        frontRight.setPower(0);
+        backLeft.setPower(0);
+        backRight.setPower(0);
+
+        //sleep for a bit to make sure the robot doesn't over shoot
+        sleep(1000);
+
+        resetAngle();
+    }
+
+
+    //this method resets the angle so that the robot's heading is now 0
+    public void resetAngle() {
+        lastAngles = imu.getAngularOrientation(AxesReference.INTRINSIC, AxesOrder.ZYX, AngleUnit.DEGREES);
+
+        globalAngle = 0;
+    }
+
+
 }
+
+

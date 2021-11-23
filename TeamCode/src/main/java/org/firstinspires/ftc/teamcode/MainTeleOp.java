3748419f86894a70f409564b0c14635a94c12a1f
Krithish Ayyappan <89314955+KrispChips@users.noreply.github.com>
2021-11-23 16:23:08 -0500

    Update MainTeleOp.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MainTeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MainTeleOp.java
@@ -4,6 +4,7 @@ import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.util.ElapsedTime;
 import com.qualcomm.robotcore.util.Range;
 
 @TeleOp(name = "DriveTrainTestLinearOp")
@@ -13,6 +14,17 @@ public class MainTeleOp extends LinearOpMode {
     private DcMotor frontRight;
     private DcMotor backLeft;
     private DcMotor backRight;
+    private DcMotor spinner;
+
+    static final double COUNTS_PER_MOTOR_REV = 1120;
+    static final double DRIVE_GEAR_REDUCTION = 2.0;
+    static final double WHEEL_DIAMETER_INCHES = 5.75;
+    static final double COUNTS_PER_INCH = (COUNTS_PER_MOTOR_REV * DRIVE_GEAR_REDUCTION) /
+            (WHEEL_DIAMETER_INCHES * 3.1415);
+
+
+    private ElapsedTime runtime = new ElapsedTime();
+
 
     @Override
     public void runOpMode() throws InterruptedException {
@@ -29,9 +41,22 @@ public class MainTeleOp extends LinearOpMode {
         frontRight.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
         backLeft.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
         backRight.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        spinner.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
 
         frontLeft.setDirection(DcMotor.Direction.REVERSE);
         backLeft.setDirection(DcMotor.Direction.REVERSE);
+
+
+        // encoder use
+        frontLeft.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        frontRight.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        backLeft.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        backRight.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+
+        frontLeft.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        frontRight.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        backLeft.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        backRight.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
 
         double minPower = -.75;
         double maxPower = .75;
@@ -46,7 +71,38 @@ public class MainTeleOp extends LinearOpMode {
             frontRight.setPower(Range.clip(-gamepad1.right_stick_y, minPower, maxPower));
             backRight.setPower(Range.clip(-gamepad1.right_stick_y, minPower, maxPower));
 
+            if (gamepad1.b) {
+
+                moveDuck(.75);
+
+
+            }
+
         }
 
     }
+
+    public void moveDuck(double power) {
+
+        int target;
+
+        if (opModeIsActive()) {
+
+            target = spinner.getCurrentPosition() + (int) (5.25 * COUNTS_PER_INCH);
+
+            spinner.setTargetPosition(target);
+
+            spinner.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+
+            runtime.reset();
+
+            spinner.setPower(Math.abs(power));
+
+            spinner.setPower(0);
+
+            spinner.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        }
+
+    }
+
 }

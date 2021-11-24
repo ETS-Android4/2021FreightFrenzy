ec1eb96915c4bfb5980854c62dcf2d0e1137f7a2
Devam (Dodesimo) <devammondal06@gmail.com>
2021-11-24 17:11:35 -0500

    Perfect gear reduction value determined (by averaging gear-reductions and then some guess-and-check)

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MainAutonomous.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MainAutonomous.java
@@ -36,7 +36,7 @@ public class MainAutonomous extends LinearOpMode {
     private ElapsedTime     runtime = new ElapsedTime();
 
     static final double     COUNTS_PER_MOTOR_REV    = 537.6 ;
-    static final double     DRIVE_GEAR_REDUCTION    = 1.23;
+    static final double     DRIVE_GEAR_REDUCTION    = 1.23; //perfect value
     static final double     WHEEL_DIAMETER_INCHES   = 4.724 ;
     static final double     COUNTS_PER_INCH         = (COUNTS_PER_MOTOR_REV * DRIVE_GEAR_REDUCTION) /
             (WHEEL_DIAMETER_INCHES * 3.14159265);

f6391a2f643381b8ae7a50bc538e9ee417a03c9c
KrispChips <krithishAyyappan@gmail.com>
2021-12-12 19:06:47 -0500

    Barcode init test

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BarcodeInitTest.java
@@ -0,0 +1,27 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+@Autonomous(name = "New Barcode Test", group = "Test")
+@Disabled
+public class BarcodeInitTest extends LinearOpMode {
+
+    @Override
+    public void runOpMode( ) throws InterruptedException {
+
+        BarcodeUtil detector = new BarcodeUtil( hardwareMap, "webcam", telemetry );
+        detector.init();
+
+        telemetry.addLine( "Image Detection init finished" );
+        telemetry.update( );
+
+        waitForStart( );
+
+        telemetry.addLine( "Position: " + detector.getBarcodePosition( ) );
+
+        while( !isStopRequested() );
+
+    }
+}
